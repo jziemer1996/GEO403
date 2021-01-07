f06f5983cf80ddcc100c3b09f04208ccc6c6752f
jziemer1996 <jz199623@gmail.com>
2021-01-07 12:17:38 +0100

    added first scripts

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+Main function to initialize variables, directories and user data and for calling/executing the desired functions
+Authors: Marlin Mueller <marlin.markus.mueller@uni-jena.de>, Jonas Ziemer <jonas.ziemer@uni-jena.de>
+"""
+
+###########################################################
+# imports
+
+import os
+import numpy as np
+###########################################################
+# project internal imports
+
+from test import *
+
+###########################################################
+###########################################################
+
+
+def main():
+    test(word="blabla")
+
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,2 @@
+def test(word):
+    print(word)
